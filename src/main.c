cdf0f07d8eab26184a25c7896736d727b8ed3465
test <test>
2024-08-21 13:47:57 +0000

    argp parsing complete

--- src/main.c
+++ src/main.c
@@ -1,126 +1,135 @@
 #include <stdio.h>
+#include <argp.h>
 #include <pcap.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
 
-/* Finds the payload of a TCP/IP packet */
-void my_packet_handler(
-    u_char *args,
-    const struct pcap_pkthdr *header,
-    const u_char *packet
-)
+static char doc[] = "Example: pcap_parser <filename> --dstaddr=192.168.0.1 --srcport=80";
+static char args_doc[] = "<filename>";
+
+static struct argp_option options[] = {
+    {"srcaddr",  's', "<ip-addr>", 0,  "set filter for source address " },
+    {"dstaddr",  'd', "<ip-addr>", 0,  "set filter for destination address" },
+    {"srcport",  'p', "<port>", 0,  "set filter for source port" },
+    {"dstport",  'o', "<port>", 0,  "set filter for destination port" },
+    { 0 }
+};
+
+struct arguments
 {
-    /* First, lets make sure we have an IP packet */
+  char *input;
+  int srcport;
+  int dstport;
+  char *srcaddr;
+  char *dstaddr;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = state->input;
+
+  switch (key)
+    {
+    case 'p':
+      arguments->srcport = atoi(arg);
+      break;
+    case 'o':
+      arguments->dstport = atoi(arg);
+      break;
+    case 's':
+      arguments->srcaddr = arg;
+      break;
+    case 'd':
+      arguments->dstaddr = arg;
+      break;
+
+    case ARGP_KEY_ARG:
+      if (state->arg_num == 0) {
+        arguments->input = arg;
+      } else {
+        return ARGP_ERR_UNKNOWN;
+      }
+      break;
+
+    case ARGP_KEY_END:
+      if (state->argc < 2){
+          argp_usage(state);
+      }
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+bool IsTCPPacket(const u_char *packet, struct pcap_pkthdr *header) {
     struct ether_header *eth_header;
     eth_header = (struct ether_header *) packet;
     if (ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
-        printf("Not an IP packet. Skipping...\n\n");
-        return;
+        return false;
     }
 
-    /* The total packet length, including all headers
-       and the data payload is stored in
-       header->len and header->caplen. Caplen is
-       the amount actually available, and len is the
-       total packet length even if it is larger
-       than what we currently have captured. If the snapshot
-       length set with pcap_open_live() is too small, you may
-       not have the whole packet. */
-    printf("Total packet available: %d bytes\n", header->caplen);
-    printf("Expected packet size: %d bytes\n", header->len);
-
-    /* Pointers to start point of various headers */
     const u_char *ip_header;
-    const u_char *tcp_header;
-    const u_char *payload;
 
-    /* Header lengths in bytes */
     int ethernet_header_length = 14; /* Doesn't change */
-    int ip_header_length;
-    int tcp_header_length;
-    int payload_length;
-
-    /* Find start of IP header */
     ip_header = packet + ethernet_header_length;
-    /* The second-half of the first byte in ip_header
-       contains the IP header length (IHL). */
-    ip_header_length = ((*ip_header) & 0x0F);
-    /* The IHL is number of 32-bit segments. Multiply
-       by four to get a byte count for pointer arithmetic */
-    ip_header_length = ip_header_length * 4;
-    printf("IP header length (IHL) in bytes: %d\n", ip_header_length);
 
-    /* Now that we know where the IP header is, we can 
-       inspect the IP header for a protocol number to 
-       make sure it is TCP before going any further. 
-       Protocol is always the 10th byte of the IP header */
     u_char protocol = *(ip_header + 9);
     if (protocol != IPPROTO_TCP) {
-        printf("Not a TCP packet. Skipping...\n\n");
-        return;
+        return false;
     }
-
-    /* Add the ethernet and ip header length to the start of the packet
-       to find the beginning of the TCP header */
-    tcp_header = packet + ethernet_header_length + ip_header_length;
-    /* TCP header length is stored in the first half 
-       of the 12th byte in the TCP header. Because we only want
-       the value of the top half of the byte, we have to shift it
-       down to the bottom half otherwise it is using the most 
-       significant bits instead of the least significant bits */
-    tcp_header_length = ((*(tcp_header + 12)) & 0xF0) >> 4;
-    /* The TCP header length stored in those 4 bits represents
-       how many 32-bit words there are in the header, just like
-       the IP header length. We multiply by four again to get a
-       byte count. */
-    tcp_header_length = tcp_header_length * 4;
-    printf("TCP header length in bytes: %d\n", tcp_header_length);
-
-    /* Add up all the header sizes to find the payload offset */
-    int total_headers_size = ethernet_header_length+ip_header_length+tcp_header_length;
-    printf("Size of all headers combined: %d bytes\n", total_headers_size);
-    payload_length = header->caplen -
-        (ethernet_header_length + ip_header_length + tcp_header_length);
-    printf("Payload size: %d bytes\n", payload_length);
-    payload = packet + total_headers_size;
-    printf("Memory address where payload begins: %p\n\n", payload);
-
-    /* Print payload in ASCII */
-    /*  
-    if (payload_length > 0) {
-        const u_char *temp_pointer = payload;
-        int byte_count = 0;
-        while (byte_count++ < payload_length) {
-            printf("%c", *temp_pointer);
-            temp_pointer++;
-        }
-        printf("\n");
-    }
-    */
-
-    return;
+    return true;
 }
 
-int main(int argc, char **argv) {    
-    char error_buffer[PCAP_ERRBUF_SIZE];
-    /* Snapshot length is how many bytes to capture from each packet. This includes*/
-    /* End the loop after this many packets are captured */
-    int total_packet_count = 200;
-    u_char *my_arguments = NULL;
+int main(int argc, char **argv) 
+{
+    struct arguments arguments;
 
-    // handle = pcap_open_live(device, snapshot_length, 0, 10000, error_buffer);
-    pcap_t *handle = pcap_open_offline(argv[1], error_buffer);
-    pcap_loop(handle, total_packet_count, my_packet_handler, my_arguments);
+    /* Default values. */
+    arguments.srcport = 0;
+    arguments.dstport = 0;
+    arguments.srcaddr = "\0";
+    arguments.dstaddr = "\0";
+    arguments.input = "\0";
 
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    // printf("input %s\n",arguments.input);
+    // printf("srcaddr %s\n",arguments.srcaddr);
+    // printf("dstaddr %s\n",arguments.dstaddr);
+    // printf("srcport %d\n",arguments.srcport);
+    // printf("dstport %d\n",arguments.dstport);
+
+    unsigned int packet_counter = 0;
+    unsigned int tcp_packet_counter = 0;
+    struct pcap_pkthdr header; 
+    const u_char *packet;
+
+    pcap_t *handle; 
+    char errbuf[PCAP_ERRBUF_SIZE];  
+    handle = pcap_open_offline(arguments.input, errbuf); 
+
+    if (handle == NULL) {
+        fprintf(stderr,"Couldn't open pcap file %s: %s\n", arguments.input, errbuf); 
+        return(2); 
+    } 
+
+    while (packet = pcap_next(handle, &header)) { 
+        if (IsTCPPacket(packet, &header)) {
+            tcp_packet_counter++;
+        }
+        packet_counter++;
+    } 
+    pcap_close(handle);
+
+    unsigned int filtered_packet_counter = 0;
+
+    printf("Number of packets in file: %d\n", packet_counter);
+    printf("Number TCP of packets in file: %d\n", tcp_packet_counter);
     return 0;
 }
-// int main(int argc, char *argv[])
-// {
-// 	char error_buffer[PCAP_ERRBUF_SIZE];
-// 	pcap_t *handle = pcap_open_offline(argv[1], error_buffer);
-
-// 	// printf("Total packet available: %d bytes\n", handle->);
-// 	pcap_loop(handle, 0, my_packet_handler, NULL);
-// 	pcap_close(handle);
-// 	return(0);
-// }
